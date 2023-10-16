65e2cd27cd6c3b146ee670e99a8a1bc8f28a9b5d
huiung <gmldnd0418@gmail.com>
2023-10-17 02:11:46 +0900

    Add image loading placeholder

--- app/src/main/java/com/hu/loldex/ui/utils/ChampionImage.kt
+++ app/src/main/java/com/hu/loldex/ui/utils/ChampionImage.kt
@@ -4,8 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.hu.loldex.R
 
 /*
  * Designed and developed by 2023 huiung
@@ -38,5 +40,6 @@ fun ChampionImage(
         contentDescription = contentDescription,
         modifier = modifier,
         contentScale = contentScale,
+        placeholder = painterResource(id = R.drawable.champion_placeholder),
     )
 }
