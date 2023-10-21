91dc16e63e7a40de7d5851c3df00bcbfd8ddf66e
huiung <gmldnd0418@gmail.com>
2023-10-22 01:32:18 +0900

    Add Color

--- app/src/main/java/com/hu/loldex/ui/theme/Color.kt
+++ app/src/main/java/com/hu/loldex/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Green300 = Color(0xFF2EB688)
+val Green500 = Color(0xFF046D4A)
+
+val Red300 = Color(0xFFF33736)
+val Red500 = Color(0xFF9C2221)
+
+val Blue300 = Color(0xFF54B1DF)
+val Blue500 = Color(0xFF1E3DA8)
+
+val Yellow300 = Color(0xFFF1A22C)
+val Yellow500 = Color(0xFFCB5C0D)
